3812b053582789f74ff2f130884b15f6e7fcb5e6
Marcus Ureta <silvergreat0@gmail.com>
2026-08-07 02:48:33 +0800

    added copyright + taskbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="font-['Arial'] mt-2 text-right mr-3">© 2026 Marcus Ureta</h1>
+
+
+      {/* DESKTOP TASKBAR */}
+      <footer>
+        <div className="ml-2 flex-2/3 hidden sm:block">
+          <p>[Opened Desktop Tabs]</p>
+        </div>
+
+        <div className="flex-1/3 justify-end content-end">
+          <p>[BUG REPORT, MUTE, & TIME]</p>
+        </div>
+      </footer>
     </>
   )
 }
