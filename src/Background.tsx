9e81b2f3a53ce005d6923896a02f885cb0340b63
Marcus Ureta <silvergreat0@gmail.com>
2026-08-08 18:11:51 +0800

    star rotation hotfix
    - star rotation does not reset when hovered over on

--- src/Background.tsx
+++ src/Background.tsx
@@ -3,14 +3,24 @@ import filled_star from './assets/filled star.svg'
 
 import {generateStarCount, generateStarLocation, generateStarRot} from './backgroundUtils'   
 import { useState, useEffect } from "react"
-import React from "react"
 
 const spacing = 96;
 
 function Background() {
     const [starCount, setStarCount] = useState<number>(generateStarCount(spacing, window.innerWidth, window.innerHeight));
     const [starHover, setHover] = useState<number | null>(null);
+    const [stars, setStars] = useState(() => {
+        let starData : number[] = [];
 
+        for(let i = 0; i < starCount; i++)
+        {
+            let starRotNumber = Math.ceil(generateStarRot());
+            console.log(starRotNumber);
+            starData.push(starRotNumber);
+        }
+
+        return starData;
+    })
 
     useEffect(() => {
         const handleResize = () => {
@@ -23,25 +33,17 @@ function Background() {
         };
     }, []);
 
-    let stars: (React.JSX.Element[]) = [];
-
-    for(let i = 0; i < starCount; i++)
-    {
-        let starRotation = 'rotate-[' + Math.ceil(generateStarRot()) + 'deg]';
-        console.log(starRotation);
-
-        stars.push(
-            <img key={i} className="animate-[spin_45s_infinite] my-10 mx-8" 
-            style={{ transform: `rotate(${Math.ceil(generateStarRot())}deg)`}} src={starHover == i ? filled_star : stroked_star}
-            onMouseEnter={() => setHover(i)} onMouseLeave={() => setHover(null)}/>
-        )
-    }
-
     return(
         <>
             <div className="absolute w-screen h-screen overflow-hidden">
                 <div className="absolute min-w-[125%] min-h-[125%] flex flex-wrap justify-center items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                    {stars}
+                    {stars.map((star, i) => (
+                        <img
+                            key={i} className="animate-[spin_45s_infinite] my-10 mx-8" 
+                            style={{ transform: `rotate(${star}deg)`}} src={starHover == i ? filled_star : stroked_star}
+                            onMouseEnter={() => setHover(i)} onMouseLeave={() => setHover(null)}
+                        />
+                    ))}
                 </div>
             </div>
         </>
