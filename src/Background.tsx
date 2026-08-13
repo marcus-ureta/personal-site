1c9698c5de5fb0a7050cb3022128a69a61f8cdaa
Marcus Ureta <silvergreat0@gmail.com>
2026-08-13 20:41:18 +0800

    moving background implemented

--- src/Background.tsx
+++ src/Background.tsx
@@ -3,7 +3,7 @@ import filled_star from './assets/filled star.svg'
 
 import './Background.css'
 import {generateStarCount, generateStarLocation, generateStarRot} from './backgroundUtils'   
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 const spacing = 96;
 
@@ -41,13 +41,47 @@ function Background() {
         return allStars;
     })
 
+    const starsRef = useRef<StarData[]>(stars);
+    const starElementsRef = useRef<(HTMLImageElement | null)[]>([]);
+
     useEffect(() => {
-        let frameID: number | null = null;
+        let resizeFrameID: number | null = null;
+        let animFrameID : number;
+
+        const handleAnim = () => {
+            starsRef.current.forEach((star, i) => {
+                let isMargined = star.starY / spacing;
+
+                if(star.starX > window.innerWidth + 16 && isMargined % 2 != 0)
+                    star.starX = -spacing;
+                else if(star.starX > window.innerWidth && isMargined % 2 == 0)
+                    star.starX = -spacing / 2;
+                
+                star.starX += 0.1;
+
+                // Rotate the star
+                star.starRotation += 0.1;
+
+                const element = starElementsRef.current[i];
+
+                if (!element) return;
+                
+                element.style.transform =
+                    `translate3d(
+                        ${star.starX}px,
+                        ${star.starY}px,
+                        0
+                    )
+                    rotate(${star.starRotation}deg)`;
+            });
+
+            animFrameID = requestAnimationFrame(handleAnim);
+        }
 
         const handleResize = () => {
-            if(frameID !== null) return;
-            frameID = requestAnimationFrame(() => {
-                frameID = null;
+            if(resizeFrameID !== null) return;
+            resizeFrameID = requestAnimationFrame(() => {
+                resizeFrameID = null;
                 const width = window.innerWidth;
                 const height = window.innerHeight;
 
@@ -74,12 +108,13 @@ function Background() {
                             newStars.push(newStar);
                         }
 
+                        starsRef.current = newStars;
                         return newStars;
                     }
                     else if(starCount < prevStars.length)
                     {
-                        newStars.filter((num) => num.starX > width || num.starY > height)
-
+                        newStars.filter((num) => num.starX > width || num.starY > height);
+                        starsRef.current = newStars;
                         return newStars;
                     }
 
@@ -89,12 +124,14 @@ function Background() {
             })
         };
 
+        animFrameID = requestAnimationFrame(handleAnim);
         window.addEventListener("resize", handleResize);
         return () => {
+            cancelAnimationFrame(animFrameID);
             window.removeEventListener("resize", handleResize);
 
-            if (frameID !== null) {
-                cancelAnimationFrame(frameID);
+            if (resizeFrameID !== null) {
+                cancelAnimationFrame(resizeFrameID);
             }
         };
     }, []);
@@ -104,13 +141,8 @@ function Background() {
             <div className="absolute w-screen h-screen overflow-hidden top-0 -z-1">
                 {stars.map((star, i) => (
                     <img
-                        key={i} className="animate-[spin_45s_infinite_alternate] mx-8 select-none" 
-                        style={{
-                            position: `absolute`,
-                            transform: `rotate(${star.starRotation}deg)`,
-                            top: `${star.starY}px`,
-                            left: `${star.starX}px`
-                        }} 
+                        key={i} className="mx-8 select-none" ref={(element) => {starElementsRef.current[i] = element; }} 
+                        style={{ position: `absolute`, willChange: "transform"}} 
                         src={starHover == i ? filled_star : stroked_star}
                         onMouseEnter={() => setHover(i)} onMouseLeave={() => setHover(null)}
                     />
