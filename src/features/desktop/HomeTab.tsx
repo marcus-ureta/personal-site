38fcc188e69a97eabe365f139c4e1892bba0abde
Marcus Ureta <silvergreat0@gmail.com>
2026-08-18 02:23:56 +0800

    styling tab

--- src/features/desktop/HomeTab.tsx
+++ src/features/desktop/HomeTab.tsx
@@ -5,7 +5,7 @@ import TabHeader from "./TabHeader"
 
 function HomeTab(){
     return(
-        <div className='w-[70vw] h-[60vh] bg-primary-blue flex flex-col justify-center items-center'>
+        <div className='w-[70vw] h-[60vh] bg-primary-blue flex flex-col justify-start items-center shadow-[inset_0_0_0_4px] shadow-accent-teal rounded-[9px]'>
             <TabHeader icon={icon} name='home' isDraggable={false}/>
             <h1>welcome!</h1>
             <h1>i'm Marcus</h1>
--- src/features/desktop/TabHeader.tsx
+++ src/features/desktop/TabHeader.tsx
@@ -7,11 +7,12 @@ interface HeaderProps{
 
 export const TabHeader = ({icon, name, isDraggable = true}: HeaderProps) => {
     
+    if(isDraggable) console.log('do something');
     
     return(
-        <div className="flex">
-            <img src={icon} className="w-[48px] h-[48px]"/>
-            <h2>{name}</h2>
+        <div className="flex w-full bg-secondary-blue h-fit py-2 rounded-tl-2xl rounded-tr-2xl">
+            <img src={icon} className="w-[40px] h-[40px] object-cover mx-4"/>
+            <h2 className="text-white">{name}</h2>
         </div>
     )
 }
