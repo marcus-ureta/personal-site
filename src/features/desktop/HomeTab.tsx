d1aaaaea09e004621143601fd3ba8a192c4a833c
Marcus Ureta <silvergreat0@gmail.com>
2026-08-18 19:17:58 +0800

    refinement of mobile and general layout of home tab

--- src/features/desktop/HomeTab.tsx
+++ src/features/desktop/HomeTab.tsx
@@ -10,17 +10,27 @@ function HomeTab(){
                 <TabHeader icon={icon} name='home' isDraggable={false}/>
 
                 {/* TAB COMPONENTS */}
-                <div className="grid grid-cols-3 gap-x-3 mt-[2.5%]">
-                    <div className="flex flex-col justify-start col-span-2">
-                        <h1 className="text-secondary-blue text-[92px]">welcome!</h1>
-                        <h1 className="text-secondary-blue text-[92px]">i'm <span style={{ color: "#F39A5A" }}>Marcus</span></h1>
-                        <h2 className="text-secondary-blue text-[40px] italic font-['Arial'] font-[550] tracking-tighter">college student and programmer</h2>
+                <div className="sm:grid sm:grid-cols-3 gap-x-3 mt-[2.5%] justify-between w-full h-full">
+                    <div className="flex flex-col col-span-2 text-center sm:text-start mx-[7%]">
+                        <h1 className="text-secondary-blue text-[92px] mt-3">welcome!</h1>
+                        <h1 className="text-secondary-blue text-[92px] leading-none">i'm <span style={{ color: "#F39A5A" }}>Marcus</span></h1>
+                        <h2 className="text-secondary-blue text-[40px] italic font-['Arial'] font-[550] tracking-[-0.06em] leading-loose mt-4 hidden sm:block">college student and programmer</h2>
                     </div>
 
-                    <div className="w-32 h-32 sm:w-96 sm:h-96 rounded-full bg-secondary-blue flex items-center justify-center text-[#2c384b] text-[3100%] font-bold overflow-hidden"><p className="translate-y-2 font-['Arial']">X</p></div>
+                    <div className="w-32 h-32 sm:w-88 sm:h-88 rounded-full bg-secondary-blue flex items-center justify-center text-[#2c384b] text-[3100%] font-bold overflow-hidden"><p className="translate-y-2 font-['Arial']">X</p></div>
+                    <h2 className="text-secondary-blue text-[40px] italic font-['Arial'] font-[550] tracking-[-0.06em] block sm:hidden text-center">college student and programmer</h2>
                 </div>
 
-                <p>yo whats up danger</p>
+                {/*  ICONS */}
+                <div className="grid grid-cols-6 gap-x-3 gap-y-0 mt-[2.5%]">
+                    <p>icon 1</p>
+                    <p>icon 2</p>
+                    <p>icon 3</p>
+
+                    <p>icon 4</p>
+                    <p>icon 5</p>
+                    <p>icon 6</p>
+                </div>
             </div>
         </div>
     )
