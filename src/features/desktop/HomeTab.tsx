8133a179403074eaaf2433ee92d216ac6ae17e7e
Marcus Ureta <silvergreat0@gmail.com>
2026-08-18 18:52:15 +0800

    home tab layout setup

--- src/features/desktop/HomeTab.tsx
+++ src/features/desktop/HomeTab.tsx
@@ -9,15 +9,18 @@ function HomeTab(){
             <div className='w-[95vw] h-[88vh] sm:w-[70vw] sm:h-[67vh] bg-primary-blue flex flex-col justify-start items-center shadow-[inset_0_0_0_4px] shadow-accent-teal rounded-[9px] z-2'>
                 <TabHeader icon={icon} name='home' isDraggable={false}/>
 
-                <div className="">
+                {/* TAB COMPONENTS */}
+                <div className="grid grid-cols-3 gap-x-3 mt-[2.5%]">
+                    <div className="flex flex-col justify-start col-span-2">
+                        <h1 className="text-secondary-blue text-[92px]">welcome!</h1>
+                        <h1 className="text-secondary-blue text-[92px]">i'm <span style={{ color: "#F39A5A" }}>Marcus</span></h1>
+                        <h2 className="text-secondary-blue text-[40px] italic font-['Arial'] font-[550] tracking-tighter">college student and programmer</h2>
+                    </div>
 
+                    <div className="w-32 h-32 sm:w-96 sm:h-96 rounded-full bg-secondary-blue flex items-center justify-center text-[#2c384b] text-[3100%] font-bold overflow-hidden"><p className="translate-y-2 font-['Arial']">X</p></div>
                 </div>
 
-                <h1 className="text-secondary-blue text-[92px]">welcome!</h1>
-                <h1 className="text-secondary-blue text-[92px]">i'm <span style={{ color: "#F39A5A" }}>Marcus</span></h1>
-                <h2 className="text-secondary-blue text-[40px] italic font-['Arial'] font-[550]">college student and programmer</h2>
-
-                <div className="w-96 h-96 rounded-full bg-secondary-blue flex items-center justify-center text-[#2c384b] text-[3100%] font-bold overflow-hidden"><p className="translate-y-2 font-['Arial']">X</p></div>
+                <p>yo whats up danger</p>
             </div>
         </div>
     )
