ece628da237741b367acfa2c3abe154fd68e655c
Marcus Ureta <silvergreat0@gmail.com>
2026-08-19 21:55:26 +0800

    react-draggable added
    - react draggable module added
    - further implementation of TabHeader.tsx

--- src/features/desktop/TabHeader.tsx
+++ src/features/desktop/TabHeader.tsx
@@ -6,14 +6,23 @@ interface HeaderProps{
 };
 
 export const TabHeader = ({icon, name, isDraggable = true}: HeaderProps) => {
-    
-    if(isDraggable) console.log('do something');
-    
+
+    const closeTab = () => {
+        console.log("tab closed!");
+    }
+
     return(
-        <div className="flex w-full bg-secondary-blue h-fit py-2 sm:py-3 rounded-t-[9px] items-center">
-            <img src={icon} className="w-[30px] sm:w-[38px] h-auto object-cover ml-[6.65%] sm:ml-[2.65%] mr-[1.5%]"/>
-            <h2 className="text-white font-['Arial'] text-xl sm:text-2xl translate-y-0.5">{name}</h2>
-        </div>
+        isDraggable ? 
+            <div className="flex w-full bg-secondary-blue h-fit py-2 sm:py-3 rounded-t-[9px] items-center">
+                <img src={icon} className="w-[30px] sm:w-[38px] h-auto object-cover ml-[6.65%] sm:ml-[2.65%] mr-[1.5%]"/>
+                <h2 className="text-white font-['Arial'] text-xl sm:text-2xl translate-y-0.5">{name}</h2>
+                <h2 className="text-white font-['Arial'] text-xl sm:text-2xl translate-y-0.5 ml-auto mr-[3%]" onClick={closeTab}>[x]</h2>
+            </div>
+        : 
+            <div className="flex w-full bg-secondary-blue h-fit py-2 sm:py-3 rounded-t-[9px] items-center">
+                <img src={icon} className="w-[30px] sm:w-[38px] h-auto object-cover ml-[6.65%] sm:ml-[2.65%] mr-[1.5%]"/>
+                <h2 className="text-white font-['Arial'] text-xl sm:text-2xl translate-y-0.5">{name}</h2>
+            </div>
     )
 }
 
