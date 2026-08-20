a1551eb0f004283ec471eda477f22a9fc690232c
Marcus Ureta <silvergreat0@gmail.com>
2026-08-20 15:58:45 +0800

    draggable window out of bounds fix

--- src/features/desktop/tabs/AboutMe.tsx
+++ src/features/desktop/tabs/AboutMe.tsx
@@ -1,7 +1,7 @@
 
 import icon from '@icons/tab/about.svg'
 import TabHeader from "../TabHeader"
-import {getTabContainerStyle, getTabStyle} from '../tabUtils'
+import {getTabStyle} from '../tabUtils'
 import "../Desktop.css"
 
 import { useRef } from 'react';
@@ -9,13 +9,14 @@ import Draggable from 'react-draggable'
 
 function AboutMe() {
     const nodeRef = useRef(null);
+    
     return(
         <>
             {/* GRAY BACKGROUND FOR MOBILE */}
             <div className='block sm:hidden fixed w-screen h-screen bg-[#525252]/40 top-0'/>
 
-            <Draggable handle=".handle-bar" nodeRef={nodeRef} allowAnyClick={false}>
-                <div className={`w-screen h-[60vh] sm:w-[58vw] ${getTabStyle()} z-1 overflow-x-hidden`} ref={nodeRef}>
+            <Draggable handle=".handle-bar" nodeRef={nodeRef} allowAnyClick={false} bounds="body">
+                <div className={`w-screen h-[92vh] sm:h-[60vh] sm:w-[58vw] ${getTabStyle()} z-1 overflow-x-hidden`} ref={nodeRef}>
                     <TabHeader icon={icon} name='about' isDraggable={true}/>
                     <p>penis</p>
                 </div>
