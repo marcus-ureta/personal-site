57b2cd6d5c41ba800954e805c0a67141854eecef
Marcus Ureta <silvergreat0@gmail.com>
2026-08-21 21:14:50 +0800

    tab managers added

---
+++ src/features/desktop/tabManager/tabManager.ts
@@ -0,0 +1,56 @@
+
+export const Tabs = {
+    About: "About",
+    Social: "Social",
+    Board: "Board",
+    Blogs: "Blogs",
+    Contact: "Contact"
+} as const
+
+export type Tabs = typeof Tabs[keyof typeof Tabs];
+
+export const TabStatus = {
+    Open: "OPEN",
+    Closed: "CLOSED"
+}
+
+export type TabStatus = typeof TabStatus[keyof typeof TabStatus];
+
+
+export interface TabState{
+    Tab: Tabs;
+    Status: TabStatus;
+    zIndex: number;
+}
+
+export const InitialTabState: TabState[] = [
+    {
+        Tab: Tabs.About,
+        Status: TabStatus.Closed,
+        zIndex: 0 
+    },
+
+    {
+        Tab: Tabs.Social,
+        Status: TabStatus.Closed,
+        zIndex: 0 
+    },
+
+    {
+        Tab: Tabs.Board,
+        Status: TabStatus.Closed,
+        zIndex: 0 
+    },
+
+    {
+        Tab: Tabs.Blogs,
+        Status: TabStatus.Closed,
+        zIndex: 0 
+    },
+
+    {
+        Tab: Tabs.Contact,
+        Status: TabStatus.Closed,
+        zIndex: 0 
+    },
+]
