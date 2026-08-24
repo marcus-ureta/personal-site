5d4b29a5ec71d6821f8dde27979473eea9f63dc6
Marcus Ureta <silvergreat0@gmail.com>
2026-08-24 19:09:56 +0800

    icons updated

--- src/features/desktop/tabs/social_tab/Social.tsx
+++ src/features/desktop/tabs/social_tab/Social.tsx
@@ -2,7 +2,12 @@
 import Icon from '@/components/icon/Icon'
 import tab_icon from '@icons/tab/social.svg'
 
+import github from '@/assets/icons/socials/github.svg'
 import instagram from '@/assets/icons/socials/instagram.svg'
+import youtube from '@/assets/icons/socials/youtube.svg'
+import linkedin from '@/assets/icons/socials/linkedin.svg'
+import facebook from '@/assets/icons/socials/facebook.svg'
+import discord from '@/assets/icons/socials/discord.svg'
 
 import {TabTemplate, type HeaderDetails, type TabDetails} from '../TabTemplate'
 import {Tabs} from '@/features/desktop/tabManager/tabManager'
@@ -26,12 +31,12 @@ function Social() {
         <TabTemplate thisTab={Tabs.Social} headerDetails={headerDetails} tabDetails={tabDetails}>
             <div className="tab-scrollable">
                 <div className="grid grid-cols-3 gap-x-16 h-full pt-[2%] min-w-0 max-w-[80%] mx-auto">
+                    <Icon name='github' icon={github} url='https://www.instagram.com/marcus__uretaah/'/>
                     <Icon name='instagram' icon={instagram} url='https://www.instagram.com/marcus__uretaah/'/>
-                    <Icon name='instagram' icon={instagram} url='https://www.instagram.com/marcus__uretaah/'/>
-                    <Icon name='instagram' icon={instagram} url='https://www.instagram.com/marcus__uretaah/'/>
-                    <Icon name='instagram' icon={instagram} url='https://www.instagram.com/marcus__uretaah/'/>
-                    <Icon name='instagram' icon={instagram} url='https://www.instagram.com/marcus__uretaah/'/>
-                    <Icon name='instagram' icon={instagram} url='https://www.instagram.com/marcus__uretaah/'/>
+                    <Icon name='youtube' icon={youtube} url='https://www.instagram.com/marcus__uretaah/'/>
+                    <Icon name='linkedin' icon={linkedin} url='https://www.instagram.com/marcus__uretaah/'/>
+                    <Icon name='facebook' icon={facebook} url='https://www.instagram.com/marcus__uretaah/'/>
+                    <Icon name='discord' icon={discord} url='https://www.instagram.com/marcus__uretaah/'/>
                 </div>
             </div>
         </TabTemplate>
