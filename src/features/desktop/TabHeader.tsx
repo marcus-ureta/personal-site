c542f6114cc1d8c4c62a20b2ba066b0ea9145797
Marcus Ureta <silvergreat0@gmail.com>
2026-08-24 20:35:08 +0800

    hotfix enable tab closing

--- src/features/desktop/TabHeader.tsx
+++ src/features/desktop/TabHeader.tsx
@@ -1,4 +1,5 @@
 
+import { useUpdatePage } from './tabUtils';
 import {useTabManager} from '@/features/desktop/tabManager/TabManagerContext'
 import { Tabs, TabStatus} from '@/features/desktop/tabManager/tabManager'
 
@@ -11,7 +12,8 @@ interface HeaderProps{
 
 export const TabHeader = ({icon, name, isDraggable = true, tab}: HeaderProps) => {
     const { setTabStates } = useTabManager();
-    
+    const updatePage = useUpdatePage();
+
     const closeTab = (Tab? : Tabs) => {
         setTabStates((previousTabs) => {
             const newTabs = previousTabs.map((tab) => {
@@ -32,10 +34,10 @@ export const TabHeader = ({icon, name, isDraggable = true, tab}: HeaderProps) =>
     return( 
         isDraggable ?
             <>
-                <div className="handle-bar hidden sm:flex w-full bg-secondary-blue h-fit py-2 sm:py-3 rounded-t-[9px] items-center cursor-grab border-[#2c3d50] border-t-2 border-l-2 border-r-2 transition-all hover:bg-[#3b526c] hover:border-[#32465c]">
+                <div className="handle-bar hidden sm:flex w-full bg-secondary-blue h-fit py-2 sm:py-3 rounded-t-[9px] items-center cursor-grab border-[#2c3d50] border-t-2 border-l-2 border-r-2 transition-all hover:bg-[#3b526c] hover:border-[#32465c]" onClick={() => updatePage(tab!)}>
                     <img src={icon} className="w-[30px] sm:w-[38px] h-auto object-cover ml-[6.65%] sm:ml-[2.65%] mr-[1.5%] select-none pointer-events-none"/>
                     <h2 className="text-white font-['Arial'] text-xl sm:text-2xl translate-y-0.5 select-none pointer-events-none">{name}</h2>
-                    <h2 className="text-white font-['Arial'] text-xl sm:text-2xl translate-y-0.5 ml-auto mr-[3%] cursor-pointer" onClick={() => closeTab(tab)}>[x]</h2>
+                    <h2 className="text-white font-['Arial'] text-xl sm:text-2xl translate-y-0.5 ml-auto mr-[3%] cursor-pointer" onClick={(e) => {e.stopPropagation(); closeTab(tab)}}>[x]</h2>
                 </div>
 
                 <div className="flex sm:hidden w-full bg-secondary-blue h-fit py-2 sm:py-3 rounded-t-[9px] items-center cursor-grab border-[#2c3d50] border-2">
