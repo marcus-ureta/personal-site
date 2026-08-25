d947f89991b57cec5b9d592a2ca3c5972dbbb21b
Marcus Ureta <silvergreat0@gmail.com>
2026-08-25 10:47:06 +0800

    base draft and styling of contact tab

--- src/features/desktop/tabs/contact_tab/Contact.tsx
+++ src/features/desktop/tabs/contact_tab/Contact.tsx
@@ -5,7 +5,7 @@ import {TabTemplate, type HeaderDetails, type TabDetails} from '../TabTemplate'
 import {Tabs} from '@/features/desktop/tabManager/tabManager'
 
 import "@/features/desktop/Desktop.css"
-
+import './Contact.css'
 
 function Contact() {
     const headerDetails : HeaderDetails = {
@@ -14,15 +14,30 @@ function Contact() {
     }
 
     const tabDetails : TabDetails = {
-        width: 36,
+        width: 44,
         height: 50,
         leftPos: 20,
+    }
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
     }
 
     return(
         <TabTemplate thisTab={Tabs.Contact} headerDetails={headerDetails} tabDetails={tabDetails}>
             <div className="tab-scrollable">
-                <h1>penis</h1>
+                <h1>want to get in touch?</h1>
+                <h2 className="font-['Arial']">work email: <span className="font-bold">marcustimothy.ureta@gmail.com</span></h2>
+                <div>
+                    <form onSubmit={onSubmit}>
+                        <input className='contact-input' placeholder='your name' name='name'/>
+                        <input className='contact-input' placeholder='your email' name='email'/>
+                        <input className='contact-input' placeholder='email subject' name='e_subject'/>
+                        <input className='contact-input' placeholder='email details' name='e_details'/>
+                        <button type='submit'>Submit Message</button>
+                    </form>
+                </div>
+                <h3>expect a response within 2-3 business days!</h3>
             </div>
         </TabTemplate>
     )
