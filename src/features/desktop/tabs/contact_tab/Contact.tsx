449ae7e8bf86baa583d65aee7ad6920b2826a9d9
Marcus Ureta <silvergreat0@gmail.com>
2026-08-25 12:21:25 +0800

    contact form mobile port

--- src/features/desktop/tabs/contact_tab/Contact.tsx
+++ src/features/desktop/tabs/contact_tab/Contact.tsx
@@ -31,7 +31,7 @@ function Contact() {
                     <h2 className="font-['Arial'] text-secondary-blue text-[clamp(24px,2.5vw,32px)] wrap-break-word">work email: <span className="font-bold">marcustimothy.ureta@gmail.com</span></h2>
 
                     <form onSubmit={onSubmit} className='flex flex-col bg-[#F3FAFF] border-secondary-blue border-2 mt-[3%] w-full mb-[2%]'>
-                        <div className='flex flex-row ml-[2%] mt-[2%]'>
+                        <div className='flex flex-col gap-y-2 md:flex-row md:ml-[2%] mt-[2%]'>
                             <div className='flex flex-col gap-y-2 flex-1'>
                                 <input className='contact-input' placeholder='your name' name='name'/>
                                 <input className='contact-input' placeholder='your email' name='email'/>
@@ -41,9 +41,9 @@ function Contact() {
                             <input className='contact-input flex-1' placeholder='email details' name='e_details'/>
                         </div>
 
-                        <div className='flex flex-row justify-between mx-[3%] my-[2%] items-center'>
+                        <div className='flex flex-col md:flex-row gap-y-2 justify-between mx-[3%] my-[2%] items-center'>
                             <button type='submit' className="bg-[#567C8D] border-2 border-[#C8D9E6] text-white font-bold font-['Jost'] py-2 px-3 tracking-wider rounded-[16px] hover:bg-[#C8D9E6] hover:border-white hover:text-[#567C8D] transition-all mr-[3%]">Submit Message</button>
-                            <h3 className="font-['Jost'] text-secondary-blue text-[clamp(16px,2.5vw,26px)] italic select-none">expect a response within 2-3 business days!</h3>
+                            <h3 className="font-['Jost'] text-secondary-blue text-[clamp(16px,2.5vw,26px)] italic select-none text-center">expect a response within 2-3 business days!</h3>
                         </div>
                     </form>
                 </div>
