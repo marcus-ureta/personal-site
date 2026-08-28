7439a00703d27ecc2b2a799f7453e5a0c8d8cc47
Marcus Ureta <silvergreat0@gmail.com>
2026-08-28 17:26:44 +0800

    warning popup completed

--- src/features/desktop/tabs/popup_tab/Popup.tsx
+++ src/features/desktop/tabs/popup_tab/Popup.tsx
@@ -2,7 +2,13 @@
 import {TabTemplate, type HeaderDetails, type TabDetails} from '../TabTemplate'
 import {Tabs} from '@/features/desktop/tabManager/tabManager'
 
+import failure from '@/assets/popup-images/failure.webp'
+import success from '@/assets/popup-images/success.webp'
+import warning from '@/assets/popup-images/warning.webp'
+
 import "@/features/desktop/Desktop.css"
+import './Popup.css'
+
 import { usePopupManager } from '../../popupManager/popupManagerContext'
 import { PopupTabs, State } from '../../popupManager/popupManager'
 
@@ -11,8 +17,9 @@ function WarningPopup() {
 
     return(
         <>
-            <h1>be responsible!</h1>
-            <p>messages are automatically reviewed before submission. i trust you but please play nice! c:</p>
+            <img className="w-[150px] h-auto object-contain" src={warning}/>
+            <h1 className="heading">be responsible!</h1>
+            <p className="detail-text">messages are automatically reviewed before submission. i trust you but please play nice! c:</p>
         </>
     )
 }
@@ -21,7 +28,11 @@ function FailurePopup() {
     console.log('Failure!');
 
     return(
-        <h1>This is a failure!</h1>
+        <>
+            <h1 className="header-text">failed to send email :c</h1>
+            <img src={failure}/>
+            <p>error message</p>
+        </>
     )
 }
 
@@ -30,7 +41,8 @@ function SuccessPopup() {
 
     return(
         <>
-            <h1>your email has been sent!</h1>
+            <h1 className="header-text">your email has been sent!</h1>
+            <img src={success}/>
             <p>i'll reach out as soon as possible, thank you!</p>
         </>
     )
@@ -56,9 +68,11 @@ function Popup() {
     return(
         <TabTemplate thisTab={Tabs.Popup} headerDetails={headerDetails} tabDetails={tabDetails}>
             <div className="tab-scrollable">
-                {activeTab?.Tab === PopupTabs.Success ? <SuccessPopup/> : ''}
-                {activeTab?.Tab === PopupTabs.Warning ? <WarningPopup/> : ''}
-                {activeTab?.Tab === PopupTabs.Failure ? <FailurePopup/> : ''}
+                <div className='flex flex-col justify-center items-center mx-[5%] my-[3%]'>
+                    {activeTab?.Tab === PopupTabs.Success ? <SuccessPopup/> : ''}
+                    {activeTab?.Tab === PopupTabs.Warning ? <WarningPopup/> : ''}
+                    {activeTab?.Tab === PopupTabs.Failure ? <FailurePopup/> : ''}
+                </div>
             </div>
         </TabTemplate>
     )
