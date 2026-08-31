622b8723675046e7ec3aed5daeac564282c3d105
Marcus Ureta <silvergreat0@gmail.com>
2026-09-01 01:59:45 +0800

    allowed scrolling and required fields

--- src/pages/BugReportPage.tsx
+++ src/pages/BugReportPage.tsx
@@ -4,33 +4,36 @@
 function BugReportPage(){
     return(
         <>
-            <div className='w-full h-full flex flex-col items-center my-[3%]'>
-                <h1 className="font-['Jost'] text-[clamp(24px,3.5vw,48px)]">so... you found a bug eh?</h1>
-                <h2 className="font-['Jost'] text-[clamp(18px,3vw,30px)]">well, let's report it!</h2>
+            <div className="w-full h-screen overflow-y-auto overflow-x-hidden">
+                <div className='flex flex-col justify-center items-center'>
+                    <h1 className="font-['Jost'] text-[clamp(24px,3.5vw,48px)]">so... you found a bug eh?</h1>
+                    <h2 className="font-['Jost'] text-[clamp(18px,3vw,30px)]">well, let's report it!</h2>
 
-                <form className='flex flex-col w-[80%] max-w-[720px] overflow-auto'>
-                    <h3>Bug Report Title</h3>
-                    <input className='base-input' placeholder='Bug Title'/>
+                    <form className='flex flex-col w-[80%] max-w-[720px] overflow-auto'>
+                        <h3>Bug Report Title</h3>
+                        <input className='base-input' placeholder='Bug Title' required/>
 
-                    <h3>Bug Description</h3>
-                    <textarea className='base-input' placeholder='What Happened?'/>
+                        <h3>Bug Description</h3>
+                        <textarea className='base-input' placeholder='What Happened?' required/>
 
-                    <h3>Expected Output</h3>
-                    <textarea className='base-input' placeholder='What Should Happen?'/>
+                        <h3>Expected Output</h3>
+                        <textarea className='base-input' placeholder='What Should Happen?' required/>
 
-                    <h3>Steps to Reproduce the Bug</h3>
-                    <textarea className='base-input' placeholder='How to Reproduce?'/>
+                        <h3>Steps to Reproduce the Bug</h3>
+                        <textarea className='base-input' placeholder='How to Reproduce?' required/>
 
-                    <h3>Video or Screenshot Evidence</h3>
-                    <input className='base-input' placeholder='Video/Screenshot URL'/>
+                        <h3>Video or Screenshot Evidence (Optional!)</h3>
+                        <input className='base-input' placeholder='Video/Screenshot URL'/>
 
-                    <h3>Additional Information</h3>
-                    <textarea className='base-input' placeholder='Additional Information'/>
+                        <h3>Additional Information (Optional!)</h3>
+                        <textarea className='base-input' placeholder='Additional Information'/>
 
-                    <h3>Email (Optional!)</h3>
-                    <input className='base-input' placeholder='Email'/>
-                    <button className='bg-primary-blue hover:cursor-pointer'>Submit Report</button>
-                </form>
+                        <h3>Email (Optional!)</h3>
+                        <input className='base-input' placeholder='Email'/>
+
+                        <button className='bg-primary-blue hover:cursor-pointer w-fit mx-auto px-[3%] py-[0.5%] border-secondary-blue border-2 rounded-xl hover:bg-secondary-blue hover:border-primary-blue hover:text-hover-white transition-all'>Submit Report</button>
+                    </form>
+                </div>
             </div>
         </>
     )
