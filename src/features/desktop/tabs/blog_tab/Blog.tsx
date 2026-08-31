5d93f3235255e87692ee8cec5a33ef2c9dd2d9c9
Marcus Ureta <silvergreat0@gmail.com>
2026-09-01 01:14:26 +0800

    blogs content temporary

--- src/features/desktop/tabs/blog_tab/Blog.tsx
+++ src/features/desktop/tabs/blog_tab/Blog.tsx
@@ -21,7 +21,13 @@ function Blog() {
     return(
         <TabTemplate thisTab={Tabs.Blogs} headerDetails={headerDetails} tabDetails={tabDetails}>
             <div className="tab-scrollable">
-                <h1>hey</h1>
+                <div className='text-center mx-[5%] my-[8%]'>
+                    <div className='flex flex-col justify-center items-center'>
+                        <h1 className="font-['Jost'] text-3xl mb-[3%]">hey! sorry but this page is not available to view yet :C</h1>
+                        <p className="font-['Jost'] text-xl mb-[3%]">i'll let you guys know when the blogs page is finished, thanks!</p>
+                        <img src='https://i.pinimg.com/736x/be/9c/50/be9c504adfed5ea3a3ad0279a85c9dae.jpg' className='h-auto w-40'/>
+                    </div>
+                </div>
             </div>
         </TabTemplate>
     )
