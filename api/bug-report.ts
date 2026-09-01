bf953a186be031f4fb0550faec1e5239e1c0a8dd
Marcus Ureta <silvergreat0@gmail.com>
2026-09-01 22:46:43 +0800

    input validation

--- api/bug-report.ts
+++ api/bug-report.ts
@@ -7,7 +7,7 @@ export default async function handler(req: any, res: any) {
         });
     }
 
-    const { title, description, expectedOutput, stepsToReproduce, evidenceUrl, additionalInfo, email } = req.body;
+    let { title, description, expectedOutput, stepsToReproduce, evidenceUrl, additionalInfo, email } = req.body;
 
     if(!title || !description || !expectedOutput || !stepsToReproduce) {
         return res.status(400).json({
@@ -15,14 +15,42 @@ export default async function handler(req: any, res: any) {
         });
     }
 
-    // INPUT VALIDATION HERE
+    if(!evidenceUrl) evidenceUrl = "N/A";
+    if(!additionalInfo) additionalInfo = "N/A";
+    if(!email) email = "N/A";
+
+    
+    if(title.length < 5 || title.length > 100) {
+        return res.status(400).json({
+            error: title.length < 5 ? "title is too short!" : "title is too long!",
+        });
+    }
+
+    if(description.length < 10 || description.length > 1000) {
+        return res.status(400).json({
+            error: description.length < 10 ? "description is too short!" : "description is too long!",
+        });
+    }
+
+    if(expectedOutput.length < 10 || expectedOutput.length > 1000) {
+        return res.status(400).json({
+            error: expectedOutput.length < 10 ? "expected output is too short!" : "expected output is too long!",
+        });
+    }
+
+    if(stepsToReproduce.length < 10 || stepsToReproduce.length > 1000) {
+        return res.status(400).json({
+            error: stepsToReproduce.length < 10 ? "steps to reproduce is too short!" : "steps to reproduce is too long!",
+        });
+    }
+
 
     const combinedDescription = `**Description:**\n${description}\n\n**Expected Output:**\n${expectedOutput}\n\n**Steps to Reproduce:**\n${stepsToReproduce}\n\n**Evidence URL:**\n${evidenceUrl}\n\n**Additional Information:**\n${additionalInfo}\n\n**Email:**\n${email}`;
 
     try{
-        const response = await fetch('https://api.github.com/repos/marcus-ureta/personal-site/issues', {
+        await fetch('https://api.github.com/repos/marcus-ureta/personal-site/issues', {
             method: 'POST',
-            headers: {
+            headers: {  
                 'Accept': 'application/vnd.github+json',
                 'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
                 'Content-Type': 'application/json',
@@ -35,14 +63,11 @@ export default async function handler(req: any, res: any) {
             })
         });
 
-        const data = await response.json();
-        console.log('Issue created successfully:', data.html_url);
         return res.status(200).json({
             success: true,
         });
 
     }catch(e){
-        console.error(e);
         return res.status(500).json({
             error: "Internal server error",
         });
