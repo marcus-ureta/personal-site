bdd75c9b7ea54c1b74e854c8d4505cc4231a63d9
Marcus Ureta <silvergreat0@gmail.com>
2026-09-09 20:20:26 +0800

    base click effect implementation

--- src/pages/DesktopPage.tsx
+++ src/pages/DesktopPage.tsx
@@ -1,11 +1,30 @@
 
 import Background from '@/components/moving_background/Background.tsx'
 
+import Crosshair from '@/assets/crosshair.svg'
+
 import DesktopView from '@/features/desktop/DesktopView.tsx'
 import { TabManagerProvider } from '@/features/desktop/tabManager/TabManagerContext.tsx'
 import Taskbar from '@/features/taskbar/Taskbar.tsx'
+import { useState, useEffect } from 'react'
+
 
 function DesktopPage(){
+
+    const [mouseClick, setMouseClick] = useState(false);
+    const [mousePos, setPosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const handleMouseClick = (e : MouseEvent) => {
+            console.log(e.clientX + ' ' + e.clientY);
+            setPosition({x: e.clientX, y: e.clientY});
+            setMouseClick(true);
+        };
+        window.addEventListener('click', handleMouseClick);
+
+        return () => window.removeEventListener('click', handleMouseClick);
+    }, []);
+
     return(
         <>
             <Background/>
@@ -16,6 +35,13 @@ function DesktopPage(){
                 <DesktopView/>
                 <Taskbar/>
             </TabManagerProvider>
+
+            <img src={Crosshair} className={`fixed scale-60 pointer-events-none ${mouseClick ? 'block' : 'hidden'} z-50`}
+            style={{
+                top: mousePos.y,
+                left: mousePos.x,
+                transform: `translate(-80%, -75%)`,
+            }}/>
         </>
     )
 }
