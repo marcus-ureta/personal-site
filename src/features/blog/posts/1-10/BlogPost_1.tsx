41f1789fadcde941e2ea9c0ddd1c80b4c0fd2348
Marcus Ureta <silvergreat0@gmail.com>
2026-09-23 01:26:55 +0800

    base draft implementation of third section - blog post #1

--- src/features/blog/posts/1-10/BlogPost_1.tsx
+++ src/features/blog/posts/1-10/BlogPost_1.tsx
@@ -41,7 +41,14 @@ function BlogPost_1(){
             <div className='mt-7 mb-12'>
                 <h1 className="font-bold text-secondary-blue text-3xl mb-5">How to NOT Lose Motivation:</h1>
 
-                <p>hey</p>
+                <p className='mb-4'>In my experience, designing a huge website like this can be a very tedious and scary task. It's not uncommon to easily get overwhelmed by the amount of things you need to keep track off. And I can tell you with 100% certainty that this can happen because... Well, it happened to me!</p>
+                <p className='mb-4'>You see, when I was first starting out designing this site, I had NO clue what I was doing. So, after a day of starting up the project, I immediately procastinated and almost abandoned it. I'm gonna be honest with you here, I was really bogged down by the realization of just how much I had to do. In fact, I almost thought this was a nigh impossible task for me to pull off.</p>
+                <p className='mb-4'>I mean after all, not only did I not have the slightest clue on how to begin. But I didn't even KNOW how to use the tech stack I was planning to use for the site. So, I gradually lost the motivation to work on the project altogether.</p>
+                <p className='mb-4'>This was the case until around a week or two later, when one of my friends showed me their own web development project that they're working on. It was a commission for someone's portfolio site, and it looked REALLY impressive. Now, this conversation eventually led me to showcase what I had done for my own website. And in contrast, it was so barebones compared to what they'd done so far that it was kind of embarassing.</p>
+                <p className='mb-4'>But truth be told, that actually inspired me to fully finish this project. Without them showcasing their own website, I wouldn't have had the courage to take such a giant leap of faith into the abyss. I wouldn't have been asked to learn so many new things just for this one side-project. In return, out of gratitude, I also wanted to work hard to showcase a project that's worth showing off to people!</p>
+                <p className='mb-4'>So you might be asking right now: What's the point of this section? Well, I'd like to argue that THIS is the most important section of the entire blog! </p>
+                <p className='mb-4'>If there's one thing that I want you to takeaway from this blog, it's that you should always find a way to stay motivated when working on a project. Without it, no matter how grand the ideas you may have are, as long as you don't have that urge to keep pushing through despite rough patches, then your never gonna finish that project! </p>
+                <p className='mb-4'>Find your own motivation. Try occasionally showing your project off to your friends and see what their reactions are like! Or maybe you find satisfaction somewhere else, then go do that as well! The point of the matter is that you should find what makes your project worth doing. That way, you have a point of reference for why you're still continuing to work on your project.</p>
             </div>
         </>
     )
